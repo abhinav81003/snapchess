c91a08ea0ee7dae01fd8aac678084b2e49777ede
Cecil Kong <clkong@usc.edu>
2022-02-05 23:47:32 -0800

    Add navbar

---
+++ snapchat-frontend/src/Navbar.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="navbar-container">
+        <div className="navbar-left">
+            <a></a>
+        </div>
+        <div className="navbar-right"> 
+            <a></a>
+            <a></a>
+            <button></button>
+        </div>
+    </div>
+  );
+}
+
+export default App;
