774c6b8f1353bc1c1fba3fca11389ef548288d23
Wonjun Lee <jasonw0416@gmail.com>
2022-02-06 04:19:37 -0800

    better animation (snap with spectacles)

--- snapchat-frontend/src/Home.js
+++ snapchat-frontend/src/Home.js
@@ -20,15 +20,27 @@ const Home = () => {
             <ScrollPage page={1}>
                 <Animator animation={ZoomInScrollOut}>
                 {/* <span style={{ fontSize: "40px" }}>I'm FadeUpScrollOut ✨</span> */}
-                    <img src= {require('./chess-123.jpg')} />
+                    <img src= {require('./images/chess-123.jpg')} />
                 </Animator>
             </ScrollPage>
             <ScrollPage page={2}>
                 <Animator animation={FadeUp}>
-                <span style={{ fontSize: "40px" }}>I'm FadeUp ⛅️</span>
+                <span style={{ fontSize: "40px" }}>New Way to Play Chess</span>
                 </Animator>
             </ScrollPage>
-            <ScrollPage page={3}></ScrollPage>
+            <ScrollPage page={3}>
+                <Animator animation={ZoomInScrollOut}>
+                    <img src= {require('./images/spectacles.png')} />
+                </Animator>
+            </ScrollPage>
+            <ScrollPage page={4}>
+                <Animator animation={FadeUp}>
+                    <img src= {require('./images/Snapchat-logo-2.jpg')} />
+                </Animator>
+            </ScrollPage>
+            <ScrollPage page={5}>
+
+            </ScrollPage>
         </ScrollContainer>
     );
 }
