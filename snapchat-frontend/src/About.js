13e3076eb7369edbb7658b898ad74541e5a52dae
Cecil Kong <clkong@usc.edu>
2022-02-06 05:20:28 -0800

    Change home colors

--- snapchat-frontend/src/About.js
+++ snapchat-frontend/src/About.js
@@ -3,6 +3,7 @@ import './About.css';
 const About = () => {
     return (  
         <div>
+            <h1>about</h1>
             <p>This is the about page.</p>
             <p>The about page is about Abhinav.</p>
             <p>Abhinav is a poop.</p>
