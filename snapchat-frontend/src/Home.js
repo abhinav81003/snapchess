4c2b358b7dc03bafb2ca6d37410b1d44d04bf425
Wonjun Lee <jasonw0416@gmail.com>
2022-02-06 03:34:22 -0800

    animation

--- snapchat-frontend/src/Home.js
+++ snapchat-frontend/src/Home.js
@@ -1,11 +1,42 @@
 import './Home.css';
 
+import { Animator, ScrollContainer, ScrollPage, batch, Fade, FadeIn, Move, MoveIn, MoveOut, Sticky, StickyIn, ZoomIn } from "react-scroll-motion";
+
+
+const ZoomInScrollOut = batch(StickyIn(), FadeIn(), ZoomIn());
+const FadeUp = batch(Fade(), Move(), Sticky());
+
+
 const Home = () => {
     return (  
-        <div class="middle">
-            <p class="middle-text">Leap into AR</p>
-        </div> 
+        <ScrollContainer>
+            <ScrollPage page={0}>
+                <Animator animation={Fade()}>
+                <div class="middle">
+                    <p class="middle-text">Leap into AR</p>
+                </div> 
+                </Animator>
+            </ScrollPage>
+            <ScrollPage page={1}>
+                <Animator animation={ZoomInScrollOut}>
+                {/* <span style={{ fontSize: "40px" }}>I'm FadeUpScrollOut ✨</span> */}
+                    <img src= {require('./chess-123.jpg')} />
+                </Animator>
+            </ScrollPage>
+            <ScrollPage page={2}>
+                <Animator animation={FadeUp}>
+                <span style={{ fontSize: "40px" }}>I'm FadeUp ⛅️</span>
+                </Animator>
+            </ScrollPage>
+            <ScrollPage page={3}></ScrollPage>
+        </ScrollContainer>
     );
 }
  
 export default Home;
+
+/*<ScrollPage page={0}>
+                <Animator animation={Sticky()}>
+                  <Route index element={<Home/>} />
+                </Animator>
+              </ScrollPage>*/
