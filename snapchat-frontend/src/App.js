568f41d31d16f78e26123b24fc4468f3a7f9631b
Cecil Kong <clkong@usc.edu>
2022-02-06 00:11:38 -0800

    Added Layout, Home, About, Team

--- snapchat-frontend/src/App.js
+++ snapchat-frontend/src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to SnapChess!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home/>} />
+          <Route path="about" element={<About/>} />
+          <Route path="team" element={<Team/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
