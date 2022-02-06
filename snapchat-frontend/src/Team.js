c943e44c4ca5d45f10a33f1537630637cdb21dc3
Cecil Kong <clkong@usc.edu>
2022-02-06 04:19:40 -0800

    Work on teams page

--- snapchat-frontend/src/Team.js
+++ snapchat-frontend/src/Team.js
@@ -4,22 +4,35 @@ const Team = () => {
     return (  
         <div>
             <div className="top-row">
-                <img src= {require("./images/abhinav-bitmoji.jpg")} className="team-photo" alt="Photo of Abhinav"></img>
-                <p class="member-name">Abhinav Gupta</p>
-                <p class="member-desc">desc</p>
+                <img src= {require("./images/abhinav-bitmoji.png")} className="team-photo" alt="Photo of Abhinav"></img>
+                <div className="under-1"/>
+                <div className="text">
+                    <p className="member-name">Abhinav Gupta</p>
+                    <p className="member-desc">desc</p>
+                </div>
 
-                <img src= {require("./images/daniel-bitmoji.jpg")} className="team-photo" alt="Photo of Daniel"></img>
-                <p class="member-name">Daniel He</p>
-                <p class="member-desc">desc</p>
+                <img src= {require("./images/daniel-bitmoji.png")} className="team-photo" alt="Photo of Daniel"></img>
+                <div className="under-2"/>
+                <div className="text">
+                    <p className="member-name">Daniel He</p>
+                    <p className="member-desc">desc</p>
+                </div>
             </div>
-            <div className="bottom-row">
-                <img src= {require("./images/wonjun-bitmoji.jpg")} className="team-photo" alt="Photo of Wonjun"></img>
-                <p class="member-name">Wonjun Lee</p>
-                <p class="member-desc">desc</p>
 
-                <img src= {require("./images/cecil-bitmoji.jpg")} className="team-photo" alt="Photo of Cecil"></img>
-                <p class="member-name">Cecil Kong</p>
-                <p class="member-desc">desc</p>
+            <div className="bottom-row">
+                <img src= {require("./images/wonjun-bitmoji.png")} className="team-photo" alt="Photo of Wonjun"></img>
+                <div className="under-3"/>
+                <div className="text">
+                    <p className="member-name">Wonjun Lee</p>
+                    <p className="member-desc">desc</p>
+                </div>
+
+                <img src= {require("./images/cecil-bitmoji.png")} className="team-photo" alt="Photo of Cecil"></img>
+                <div className="under-4"/>
+                <div className="text">
+                    <p className="member-name">Cecil Kong</p>
+                    <p className="member-desc">desc</p>
+                </div>
             </div>
         </div>
     );
